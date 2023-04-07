5d402e968e2c8468a76d93c93a486ef5294a43f3
alexraz0 <64764374+alexraz0@users.noreply.github.com>
2023-04-06 21:48:26 -0700

    Update MotorControl.ino

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,18 +1,40 @@
+#include <Arduino.h>
 #include <SPI.h>
 #include <BH1750.h>
-#include <DHT.h>
+#include "DHT.h"
 
 // Tempurature and Humidity Sensor DHT11
 
-#define DHTPIN 8 // Digital pin connected to the DHT sensor
+#define DHTPIN 2 // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11  // DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor
 
-const int tempLowThreshold = 24;
-const int tempAVGThreshold = 25;
-const int tempHighThreshold = 
-const int humThreshold = 40;
+const int target_Temperature = 25.0; // Desired target temperature
+const int target_Humidity = 60.0; // Desired target humidity
+
+String plant_type = "tomato"; // replace with actual method of reading plant type
+
+// Hystersis values for different plant types
+// Add more types and values as needed
+float high_hysteresis_def = 1.0;
+float low_hysteresis_def = 0.0;
+
+float high_hysteresis_1 = 1.1;
+float low_hysteresis_1 = 0.1;
+
+float high_hysteresis_2 = 1.2;
+float low_hysteresis_2 = 0.2;
+
+float high_hysteresis_3 = 1.3;
+float low_hysteresis_3 = 0.3;
+
+float hysteresis[4][2] = {
+    {high_hysteresis_def, low_hysteresis_def},
+    {high_hysteresis_1, low_hysteresis_1},
+    {high_hysteresis_2, low_hysteresis_2},
+    {high_hysteresis_3, low_hysteresis_3}
+};
 
 
 // Light Sensor BH1750
@@ -22,14 +44,14 @@ BH1750 GY302; // Initialize BH1750 object
 
 
 // Pin Definition for the Shift Register (165)
-const uint8_t ISRDataPin = 2;   // connected to 74HC165 QH (9) pin
-const uint8_t ISRLatchPin = 3;  // connected to 74HC165 SH/LD (1) pin
-const uint8_t ISRClockPin = 4;  // connected to 74HC165 CLK (2) pin
+const uint8_t ISRDataPin = 3;   // connected to 74HC165 QH (9) pin
+const uint8_t ISRLatchPin = 4;  // connected to 74HC165 SH/LD (1) pin
+const uint8_t ISRClockPin = 5;  // connected to 74HC165 CLK (2) pin
 
 // Pin definition for the Shift Register (595)
-const uint8_t OSRDataPin = 5;   // connected to 74HC595 SER (14) pin
-const uint8_t OSRLatchPin = 6;  // connected to 74HC595 RCLK (12) pin
-const uint8_t OSRClockPin = 7;  // connected to 74HC595 SRCLK (11) pin
+const uint8_t OSRDataPin = 6;   // connected to 74HC595 SER (14) pin
+const uint8_t OSRLatchPin = 7;  // connected to 74HC595 RCLK (12) pin
+const uint8_t OSRClockPin = 8;  // connected to 74HC595 SRCLK (11) pin
 
 // Pin definition for the Limit Switches
 const uint8_t MOTOR1_SW_UP = 0;  // bit position for 74HC165 Motor 1 Limit Switch UP
@@ -49,25 +71,55 @@ const uint8_t shiftReg2Value = 0; // (595)
 
 void setup() {
 
-   // Serial Monitor
-   Serial.begin(9600);  // initialize serial bus
-   while (!Serial);     // wait for serial connection
+  // Serial Monitor
+  Serial.begin(9600);  // initialize serial bus
+  while (!Serial);     // wait for serial connection
+  Serial.println(F("DHT Test!"));
 
-   // 74HC165 shift register
-   pinMode(ISRDataPin, INPUT);
-   pinMode(ISRLatchPin, OUTPUT);
-   pinMode(ISRClockPin, OUTPUT);
+  // // 74HC165 shift register
+  pinMode(ISRDataPin, INPUT);
+  pinMode(ISRLatchPin, OUTPUT);
+  pinMode(ISRClockPin, OUTPUT);
 
-   // 74HC595 shift register
-   pinMode(OSRDataPin, OUTPUT);
-   pinMode(OSRLatchPin, OUTPUT);
-   pinMode(OSRClockPin, OUTPUT);
+  // // 74HC595 shift register
+  pinMode(OSRDataPin, OUTPUT);
+  pinMode(OSRLatchPin, OUTPUT);
+  pinMode(OSRClockPin, OUTPUT);
 
-    // Turn off all motors initially
-   osrWriteRegister(0);
+  // // Turn off all motors initially
+  osrWriteRegister(0);
 
-   //Initialize GY-302 module
-//   GY302.begin(); 
+  //Initialize GY-302 module
+  dht.begin(); 
+
+  //Initialize GY-302 module
+  GY302.begin(); 
+
+
+
+  float high_hysteresis_plant;
+  float low_hysteresis_plant;
+
+  // Get Hysteresis values for the plant types
+  if (plant_type == "tomato") {
+    float high_hysteresis_plant = hysteresis[1][0];
+    float low_hysteresis_plant = hysteresis[0][1];
+  }
+  else if (plant_type == "lettuce") {
+    high_hysteresis_plant = hysteresis[2][0];
+    low_hysteresis_plant = hysteresis[0][2];
+  } 
+  else if (plant_type == "orange") {
+    high_hysteresis_plant = hysteresis[3][0];
+    low_hysteresis_plant = hysteresis[0][3];
+ }
+  else {
+    // Default hysteresis values if plant is not recognized
+    high_hysteresis_plant = hysteresis[0][0];
+    low_hysteresis_plant = hysteresis[0][0];
+
+  }
+   
 
 }
 
@@ -75,17 +127,52 @@ void loop() {
 
  // Read temperature and humidity from DHT sensor
 
- float tempurature = dht.readTemperature();
- float humidity = dht.readHumidity();
+ float celsius = dht.readTemperature(); // Read temperature as Celsius
+ float temperature = dht.readTemperature(true); // Read temperature as Fahrenheit (isFahrenheit = true)
+ float humidity = dht.readHumidity(); 
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(celsius) || isnan(temperature)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(temperature, humidity);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(celsius, humidity, false);
+
+ // Print Humidity and Temperature values
+  Serial.print(F("Humidity: "));
+  Serial.print(humidity);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(celsius);
+  Serial.print(F("°C "));
+  Serial.print(temperature);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));  
+
+  delay(8000);
+
+//------------------
+// Hysteresis
+// The difference between the actual value and the expected value
+//------------------
+
+ String plant_type = "tomato";
+
+ // Get Hysteresis values for the plant type
+
+
 
  // Calculate temperature difference
  float tempDiff = temperature - 25;
 
- // Calculate motor speed based on tempurature difference  
- int motorspeed = map(tempDiff, -10, 10, 0, 255); Maps tempDiff range (-10 to 10) to motorSpeed range (0 to 255)
 
-
- //   motor1Control(2);
   // Get the Reed Switches State
 
    bool MOTOR1_SW_UP_STATE = isrDigitalRead(MOTOR1_SW_UP);
@@ -97,110 +184,142 @@ void loop() {
    bool MOTOR3_SW_UP_STATE = isrDigitalRead(MOTOR3_SW_UP);
    bool MOTOR3_SW_DN_STATE = isrDigitalRead(MOTOR3_SW_DN);
 
-
+ //-----------------------------------------------------------------------------------
  // Control motor direction based on the temperature, humidity, and light intensity
-  if(temperature >= tempThreshold || humidity >= humThreshold ) { // if tempurature and humidity are higher than threshold
-    // Determine the direction of motor 1 movement
-    if (MOTOR1_SW_UP_STATE == LOW && MOTOR1_SW_DN_STATE ==  HIGH) {
-      // Move motor up
-      motor1Control(2);
-    } 
-    else {
-      motor1Control(1);
-    }
+ //-----------------------------------------------------------------------------------
+ //--------------------------------
+ // Temperature conditions 
+ // first move
+//   if( temperature_f > target_temperature + high_hysteresis_plant ) { 
+//     // Determine the direction of motor 1 movement
+   
+//     if (MOTOR1_SW_UP_STATE == LOW && MOTOR1_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor1Control(2);
+//     } 
+//     else {
+//       motor1Control(1);
+//     }
 
-    // Determine the direction of motor 1 movement
-    if (MOTOR2_SW_UP_STATE == LOW && MOTOR2_SW_DN_STATE ==  HIGH) {
-      // Move motor up
-      motor2Control(2);
-    } 
-    else {
-      motor2Control(1);
-    }
+//     // Determine the direction of motor 1 movement
+//     if (MOTOR2_SW_UP_STATE == LOW && MOTOR2_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor2Control(2);
+//     } 
+//     else {
+//       motor2Control(1);
+//     }
 
-    // Determine the direction of motor 1 movement
-    if (MOTOR3_SW_UP_STATE == LOW && MOTOR3_SW_DN_STATE ==  HIGH) {
-      // Move motor up
-      motor3Control(2);
-    } 
-    else {
-      motor3Control(1);
-    }
+//     // Determine the direction of motor 1 movement
+//     if (MOTOR3_SW_UP_STATE == LOW && MOTOR3_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor3Control(2);
+//     } 
+//     else {
+//       motor3Control(1);
+//     }
 
-    delay(100);
- }
+//     delay(100);
+//  }
 
- if(temperature >= tempThreshold || humidity >= humThreshold ) { // MEDIAN TEMPURTURE AND HUMIDITY
-  // Determine the direction of motor 1 movement
-  if (MOTOR1_SW_UP_STATE == HIGH && MOTOR1_SW_DN_STATE ==  LOW) {
-    // Move motor up
-   motor1Control(2);
-  } 
-  else {
-    motor1Control(1);
-  }
+// // Second move
+//  else if(temperature_f < target_temperature - low_hysteresis_plant ) { // if tempurature and humidity are higher than hysteresis
+//   // Determine the direction of motor 1 movement
+//   if (MOTOR1_SW_UP_STATE == HIGH && MOTOR1_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor1Control(2);
+//   } 
+//   else {
+//     motor1Control(1);
+//   }
 
-   // Determine the direction of motor 1 movement
-  if (MOTOR2_SW_UP_STATE == HIGH && MOTOR2_SW_DN_STATE ==  LOW) {
-    // Move motor up
-   motor2Control(2);
-  } 
-  else {
-    motor2Control(1);
-  }
+//    // Determine the direction of motor 1 movement
+//   if (MOTOR2_SW_UP_STATE == HIGH && MOTOR2_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor2Control(2);
+//   } 
+//   else {
+//     motor2Control(1);
+//   }
 
-   // Determine the direction of motor 1 movement
-  if (MOTOR3_SW_UP_STATE == HIGH && MOTOR3_SW_DN_STATE ==  LOW) {
-    // Move motor up
-   motor3Control(2);
-  } 
-  else {
-    motor3Control(1);
-  }
+//    // Determine the direction of motor 1 movement
+//   if (MOTOR3_SW_UP_STATE == HIGH && MOTOR3_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor3Control(2);
+//   } 
+//   else {
+//     motor3Control(1);
+//   }
 
-  delay(100);
- }
+//   delay(100);
+//  }
+//  //---------------------------
+//  // Humidity conditions 
+//  // first move
+//   else if( humidity > target_humidity + high_hysteresis_plant) { 
+//     // Determine the direction of motor 1 movement
+//     if (MOTOR1_SW_UP_STATE == LOW && MOTOR1_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor1Control(2);
+//     } 
+//     else {
+//       motor1Control(1);
+//     }
+
+//     // Determine the direction of motor 1 movement
+//     if (MOTOR2_SW_UP_STATE == LOW && MOTOR2_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor2Control(2);
+//     } 
+//     else {
+//       motor2Control(1);
+//     }
+
+//     // Determine the direction of motor 1 movement
+//     if (MOTOR3_SW_UP_STATE == LOW && MOTOR3_SW_DN_STATE ==  HIGH) {
+//       // Move motor up
+//       motor3Control(2);
+//     } 
+//     else {
+//       motor3Control(1);
+//     }
+
+//     delay(100);
+//  }
+
+// // Second move
+//  else if(humidity < target_humidity - low_hysteresis_plant)) { // if tempurature and humidity are higher than hysteresis
+//   // Determine the direction of motor 1 movement
+//   if (MOTOR1_SW_UP_STATE == HIGH && MOTOR1_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor1Control(2);
+//   } 
+//   else {
+//     motor1Control(1);
+//   }
+
+//    // Determine the direction of motor 1 movement
+//   if (MOTOR2_SW_UP_STATE == HIGH && MOTOR2_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor2Control(2);
+//   } 
+//   else {
+//     motor2Control(1);
+//   }
+
+//    // Determine the direction of motor 1 movement
+//   if (MOTOR3_SW_UP_STATE == HIGH && MOTOR3_SW_DN_STATE ==  LOW) {
+//     // Move motor up
+//    motor3Control(2);
+//   } 
+//   else {
+//     motor3Control(1);
+//   }
+
+//   delay(100);
+//  }
 
 
-  // Determine the direction of motor 1 movement
-  if (MOTOR1_SW_UP_STATE == HIGH && MOTOR1_SW_DN_STATE ==  LOW) {
-    // Move motor up
-   motor1Control(2);
-  } 
-  else {
-    motor1Control(1);
-  }
-  delay(100);
- }
-  delay(100);
- }
-  // Determine the direction of motor 1 movement
-  if (MOTOR1_SW_UP_STATE == HIGH && !MOTOR1_SW_DN_STATE == HIGH) {
-    // Move motor up
-   motor1Control(2);
-  } 
-  // else if (!MOTOR1_SW_UP_STATE == HIGH && MOTOR1_SW_DN_STATE == HIGH) {
-  //   // Move motor down
-  //  motor1Control(3);
-  // }
-  //  else {
-  //   // Stop the motor
-  //  motor1Control(1);
-  // }
-
-  // Determine the direction of motor 1 movement
-  // if (MOTOR2_SW_UP_STATE == HIGH && !MOTOR2_SW_DN_STATE == HIGH) {
-  //   // Move motor up
-  //  motor2Control(2);
-  // } 
-  // else if (!MOTOR2_SW_UP_STATE == HIGH && MOTOR2_SW_DN_STATE == HIGH) {
-  //   // Move motor down
-  //  motor2Control(3);
-  // }
-  //  else {
-  //   // Stop the motor
-  //  motor2Control(1);
-  // }
 
    // Read and print inputs at the specified sampling rate
    static unsigned long previousTime = 0;
@@ -384,5 +503,7 @@ void motor3Control(uint8_t inputvalue3){
   }
 
 }
+
+
 
 
